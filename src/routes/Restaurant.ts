250d6a7d844db4b9ce4b2b91d76e618705c7ab50
Mikoolio <mikab@moveo.co.il>
2024-01-25 13:45:44 +0200

    env to gitignore

--- src/routes/Restaurant.ts
+++ src/routes/Restaurant.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/', controller.createRestaurant);
 router.get('/:restaurantId', controller.readRestaurant);
-router.get('/', controller.readAllRestaurant);
+router.get('/', controller.readAllRestaurants);
 router.patch('/:restaurantId', controller.updateRestaurant);
 router.delete('/:restaurantId', controller.deleteRestaurant);
 
